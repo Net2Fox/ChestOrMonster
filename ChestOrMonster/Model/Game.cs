d7d17bd5cd39b47eb1e926dae9da0d3196c3da83
Net2Fox <trakounet.fox@gmail.com>
2025-10-09 11:47:46 +0300

    Созданы классы игрока и игры

---
+++ ChestOrMonster/Model/Game.cs
@@ -0,0 +1,24 @@
+﻿using ChestOrMonster.Interface;
+
+namespace ChestOrMonster.Model;
+
+public class Game : IGame
+{
+    public int CurrentStep { get; set; }
+    public StepType CurrentStepType { get; set; }
+    public IPlayer Player { get; set; }
+    
+    private static Random _random = new Random(DateTime.Now.Millisecond);
+
+    public Game(string playerName)
+    {
+        CurrentStep = 0;
+        Player = new Player(playerName);
+    }
+    
+    public void MoveStep()
+    {
+        CurrentStep++;
+        CurrentStepType = (StepType)_random.Next(1, 3);
+    }
+}
---
+++ ChestOrMonster/Model/Player.cs
@@ -0,0 +1,47 @@
+﻿using ChestOrMonster.Interface;
+using ChestOrMonster.Model.Item;
+
+namespace ChestOrMonster.Model;
+
+public class Player : IPlayer
+{
+    public string Name { get; set; }
+    public double Hp { get; set; } = _maxHp;
+    public IBaseItem? Weapon { get; set; } = null;
+    public IBaseItem? Armor { get; set; } = null;
+    
+    private static double _maxHp = 100;
+
+    public Player(string name)
+    {
+        Name = name;
+    }
+
+    public void ChangeEquipment(IBaseItem item)
+    {
+        switch (item)
+        {
+            case Armor armor:
+                Armor = armor;
+                break;
+            case Weapon weapon:
+                Weapon = weapon;
+                break;
+            case HealingPotion healingPotion:
+                Hp = _maxHp;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(item), item, null);
+        }
+    }
+    
+    public double Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Defend()
+    {
+        throw new NotImplementedException();
+    }
+}
