6dc4a3824c92576f75cb24901520e1136d8f2829
Net2Fox <trakounet.fox@gmail.com>
2025-10-09 11:48:38 +0300

    Созданы фабрики для создания случайных врагов и предметов

---
+++ ChestOrMonster/Factory/EnemyFactory.cs
@@ -0,0 +1,20 @@
+﻿using ChestOrMonster.Interface;
+using ChestOrMonster.Model.Enemy;
+
+namespace ChestOrMonster.Factory;
+
+public static class EnemyFactory
+{
+    private static Random _random = new(DateTime.Now.Millisecond);
+
+    public static IBaseEnemy CreateRandomEnemy()
+    {
+        int roll = _random.Next(0, 3);
+        return roll switch
+        {
+            0 => new Goblin(),
+            1 => new Skeleton(),
+            2 => new Mage()
+        };
+    }
+}
---
+++ ChestOrMonster/Factory/ItemFactory.cs
@@ -0,0 +1,49 @@
+﻿using ChestOrMonster.Interface;
+using ChestOrMonster.Model.Item;
+
+namespace ChestOrMonster.Factory;
+
+public static class ItemFactory
+{
+    private static Random _random = new(DateTime.Now.Millisecond);
+
+    private static readonly (string Name, double Damage)[] Weapons =
+    [
+        ("Деревянный меч", 5),
+        ("Стальной меч", 10),
+        ("Боевой топор", 12),
+        ("Длинный лук", 8),
+        ("Магический посох", 15)
+    ];
+
+    private static readonly (string Name, double Def)[] Armors =
+    [
+        ("Кожаная броня", 3),
+        ("Кольчуга", 6),
+        ("Латные доспехи", 10),
+        ("Магический плащ", 8)
+    ];
+    
+    public static IBaseItem CreateRandomItem()
+    {
+        int itemType = _random.Next(0, 3);
+        return itemType switch
+        {
+            0 => CreateRandomWeapon(),
+            1 => CreateRandomArmor(),
+            2 => new HealingPotion()
+        };
+    }
+
+    private static Weapon CreateRandomWeapon()
+    {
+        var template = Weapons[_random.Next(0, Weapons.Length)];
+        return new Weapon(template.Name, template.Damage);
+    }
+    
+    private static Armor CreateRandomArmor()
+    {
+        var template = Armors[_random.Next(0, Weapons.Length)];
+        return new Armor(template.Name, template.Def);
+    }
+}
