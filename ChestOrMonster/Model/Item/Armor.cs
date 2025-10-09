7796d7213bdd5119ecbc914f1af0cdbe52a8d238
Net2Fox <trakounet.fox@gmail.com>
2025-10-09 11:48:00 +0300

    Созданы классы оружия и доспехов

---
+++ ChestOrMonster/Model/Item/Armor.cs
@@ -0,0 +1,15 @@
+﻿using ChestOrMonster.Interface;
+
+namespace ChestOrMonster.Model.Item;
+
+public class Armor : IArmor
+{
+    public string Name { get; set; }
+    public double Def { get; set; }
+
+    public Armor(string name, double def)
+    {
+        Name = name;
+        Def = def;
+    }
+}
---
+++ ChestOrMonster/Model/Item/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using ChestOrMonster.Interface;
+
+namespace ChestOrMonster.Model.Item;
+
+public class Weapon : IWeapon
+{
+    public string Name { get; set; }
+    public double Damage { get; set; }
+
+    public Weapon(string name, double damage)
+    {
+        Name = name;
+        Damage = damage;
+    }
+}
