59ed3424dd7ac4be4538065e2f7eeb787c2d1e8c
Net2Fox <trakounet.fox@gmail.com>
2025-10-09 11:46:24 +0300

    Созданы интерфейсы базовых предметов (оружие и доспехи)

---
+++ ChestOrMonster/Interface/IArmor.cs
@@ -0,0 +1,6 @@
+﻿namespace ChestOrMonster.Interface;
+
+public interface IArmor : IBaseItem
+{
+    public double Def { get; set; }
+}
---
+++ ChestOrMonster/Interface/IWeapon.cs
@@ -0,0 +1,6 @@
+﻿namespace ChestOrMonster.Interface;
+
+public interface IWeapon : IBaseItem
+{
+    public double Damage { get; set; }
+}
