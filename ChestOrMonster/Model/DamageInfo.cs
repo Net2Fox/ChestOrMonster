c5c3547d7812cde0ac86e23d8cc5a6853da2beea
Net2Fox <trakounet.fox@gmail.com>
2025-10-14 15:15:34 +0300

    Добавлена структура информации об уроне Она хранит в себе информацию о типе урона, количестве урона, наложении эффекта

---
+++ ChestOrMonster/Model/DamageInfo.cs
@@ -0,0 +1,28 @@
+﻿namespace ChestOrMonster.Model;
+
+public struct DamageInfo
+{
+    public double Amount { get; private set; }
+    public DamageType Type { get; private set; }
+    public StatusEffect Effect { get; private set; }
+
+    public DamageInfo(double amount, DamageType type = DamageType.Physical, StatusEffect effect = StatusEffect.None)
+    {
+        Amount = amount;
+        Type = type;
+        Effect = effect;
+    }
+    
+    public void ChangeDamageAmount(double amount)
+    {
+        switch (amount)
+        {
+            case < 0:
+                Amount = 0;
+                break;
+            default:
+                Amount = amount;
+                break;
+        }
+    }
+}
