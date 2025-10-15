9a39b2387e0af6df6feaa4bdf5678bc2de65499e
Net2Fox <trakounet.fox@gmail.com>
2025-10-15 14:01:11 +0300

    Реализовано создание случайного босса

--- ChestOrMonster/Factory/EnemyFactory.cs
+++ ChestOrMonster/Factory/EnemyFactory.cs
@@ -1,6 +1,7 @@
 ﻿using ChestOrMonster.Interface;
 using ChestOrMonster.Model;
 using ChestOrMonster.Model.Enemy;
+using ChestOrMonster.Model.Enemy.Boss;
 
 namespace ChestOrMonster.Factory;
 
@@ -16,6 +17,17 @@ public static class EnemyFactory
             0 => new Goblin(),
             1 => new Skeleton(),
             2 => new Mage()
+        };
+    }
+
+    public static BaseEntity CreateRandomBoss()
+    {
+        int roll = _random.Next(0, 3);
+        return roll switch
+        {
+            0 => new Orc(),
+            1 => new GiantSkeleton(),
+            2 => new Archmage()
         };
     }
 }
