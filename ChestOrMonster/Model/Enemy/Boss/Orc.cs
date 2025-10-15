c4201a2fa5b5a48623f30e4b82986da2197cb305
Net2Fox <trakounet.fox@gmail.com>
2025-10-15 14:01:25 +0300

    Удалена ненужная перезапись у Орка

--- ChestOrMonster/Model/Enemy/Boss/Orc.cs
+++ ChestOrMonster/Model/Enemy/Boss/Orc.cs
@@ -6,6 +6,5 @@ public class Orc : Goblin
     public override double Hp => base.Hp * 2;
     public override double Atk => base.Atk * 1.5;
     public override double Def => base.Def * 1.2;
-    public override DamageType AttackType => DamageType.Usual;
     protected override double CritRate => base.CritRate + 0.1;
 }
