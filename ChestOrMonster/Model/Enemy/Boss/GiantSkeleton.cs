6e95f353d9130c1be553f1f72e103ae8f72fa7e0
Net2Fox <trakounet.fox@gmail.com>
2025-10-15 14:00:57 +0300

    Добавлен босс "Гигантский скелет"

---
+++ ChestOrMonster/Model/Enemy/Boss/GiantSkeleton.cs
@@ -0,0 +1,9 @@
+﻿namespace ChestOrMonster.Model.Enemy.Boss;
+
+public class GiantSkeleton : Skeleton
+{
+    public override string Name => "Гигантский скелет";
+    public override double Hp => base.Atk * 2.5;
+    public override double Atk => base.Atk * 1.3;
+    public override double Def => base.Def * 1.4;
+}
