b580217fb7462066241e3d58190309fff3e04dc5
Net2Fox <trakounet.fox@gmail.com>
2026-02-27 10:51:26 +0300

    feat(app): добавлено DI

--- ChestOrMonster.WPF/App.xaml.cs
+++ ChestOrMonster.WPF/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using ChestOrMonster.WPF.Model;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ChestOrMonster.WPF;
 
@@ -9,4 +11,23 @@ namespace ChestOrMonster.WPF;
 /// </summary>
 public partial class App : Application
 {
+   public new static App Current => (App)Application.Current;
+   
+   public IServiceProvider Services { get; }
+
+   public App()
+   {
+      Services = ConfigureServices();
+      
+      this.InitializeComponent();
+   }
+
+   public IServiceProvider ConfigureServices()
+   {
+      var services = new ServiceCollection();
+
+      services.AddSingleton<GameScript>();
+      
+      return services.BuildServiceProvider();
+   }
 }
