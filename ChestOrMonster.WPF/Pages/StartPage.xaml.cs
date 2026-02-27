4084c3805785332365c048abf6f9d4736051f006
Net2Fox <trakounet.fox@gmail.com>
2026-02-27 09:41:43 +0300

    feat(startpage): добавлен переход на основную страницу игры

--- ChestOrMonster.WPF/Pages/StartPage.xaml.cs
+++ ChestOrMonster.WPF/Pages/StartPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace ChestOrMonster.WPF.Pages;
 
@@ -7,5 +8,10 @@ public partial class StartPage : Page
     public StartPage()
     {
         InitializeComponent();
+    }
+
+    private void StartGameButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        NavigationService?.Navigate(new MainPage());
     }
 }
