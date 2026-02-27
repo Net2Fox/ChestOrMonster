c2186373861da36684a9f6ad4fcddf7a0de30499
Net2Fox <trakounet.fox@gmail.com>
2026-02-27 10:51:02 +0300

    feat(gamescript): добавлен файл основного скрипта игры

---
+++ ChestOrMonster.WPF/Model/GameScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using ChestOrMonster.Interface;
+using ChestOrMonster.Model;
+using ChestOrMonster.Model.Event;
+using ChestOrMonster.Model.Event.Enemy;
+using ChestOrMonster.Model.Event.Player;
+using ChestOrMonster.Model.Item;
+
+namespace ChestOrMonster.WPF.Model;
+
+public class GameScript
+{
+    
+}
