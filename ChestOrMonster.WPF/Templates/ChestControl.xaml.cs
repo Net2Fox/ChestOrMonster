3ea3985c0fcbd41efba4580332ca3ff0c0a720c8
Net2Fox <trakounet.fox@gmail.com>
2026-02-27 10:45:59 +0300

    feat(templates): добавлены UserControl для сундука и боя

---
+++ ChestOrMonster.WPF/Templates/ChestControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ChestOrMonster.WPF.Templates;
+
+public partial class ChestControl : UserControl
+{
+    public ChestControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ChestOrMonster.WPF/Templates/FightControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ChestOrMonster.WPF.Templates;
+
+public partial class FightControl : UserControl
+{
+    public FightControl()
+    {
+        InitializeComponent();
+    }
+}
