08edf999cf77b7b3742e828c20e5c849b7fdf291
Net2Fox <trakounet.fox@gmail.com>
2026-03-01 13:53:22 +0300

    feat(chestcontrol): реализовано выпадение вещей из сундука

--- ChestOrMonster.WPF/Templates/ChestControl.xaml.cs
+++ ChestOrMonster.WPF/Templates/ChestControl.xaml.cs
@@ -4,9 +4,7 @@ using System.Windows.Controls;
 using ChestOrMonster.Interface;
 using ChestOrMonster.Model;
 using ChestOrMonster.Model.Item;
-using ChestOrMonster.WPF.Model;
 using ChestOrMonster.WPF.Pages;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace ChestOrMonster.WPF.Templates;
 
@@ -30,8 +28,6 @@ public partial class ChestControl : UserControl
     
     public void HandleChest(IBaseItem item)
     {
-        _mainPage.Logger.Add($"Вам выпал {item.Name}!");
-        
         switch (item)
         {
             case Weapon or Armor:
@@ -45,8 +41,7 @@ public partial class ChestControl : UserControl
     
     public void OfferHealingPotion(IBaseItem healingPotion)
     {
-        _mainPage.Logger.Add("Вы получили лечебное зелье!");
-        int choice = _mainPage.MessageBoxUserChoice($"У вас сейчас {_mainPage.GameInstance.Player.Hp:F0} HP. Хотите выпить зелье или выбросить его?");
+        int choice = _mainPage.MessageBoxUserChoice($"Хотите выпить зелье или выбросить его?");
         ItemAction action = choice == 1 ? ItemAction.Use : ItemAction.Discard;
         
         _mainPage.GameInstance.ProcessItemAction(healingPotion, action);
@@ -63,30 +58,22 @@ public partial class ChestControl : UserControl
 
     public void OfferEquipmentChange(IBaseItem equipment)
     {
-        _mainPage.Logger.Add($"Вы получили {equipment.Name}!");
         StringBuilder stringBuilder = new StringBuilder();
         
         switch (equipment)
         {
             case Weapon weapon:
                 stringBuilder.AppendLine(
-                    $"Ваши характеристики сейчас:\n{_mainPage.GameInstance.Player.Weapon.Name}, {_mainPage.GameInstance.Player.Weapon.Damage}.");
-                stringBuilder.AppendLine(
                     $"Характеристики нового оружия: {weapon.Name}, {weapon.Damage}");
-                stringBuilder.AppendLine("Хотите сменить оружие или оставить текущее?");
+                stringBuilder.AppendLine("Хотите сменить оружие?");
                 break;
                 
             case Armor armor:
                 stringBuilder.AppendLine(
-                    $"Ваши характеристики сейчас:\n{_mainPage.GameInstance.Player.Armor.Name}, {_mainPage.GameInstance.Player.Armor.Def}.");
-                stringBuilder.AppendLine(
                     $"Характеристики новых доспехов: {armor.Name}, {armor.Def}");
-                stringBuilder.AppendLine("Хотите сменить доспехи или оставить текующие?");
+                stringBuilder.AppendLine("Хотите сменить доспехи?");
                 break;
         }
-        
-        stringBuilder.AppendLine("\t1. Сменить\t2. Оставить");
-        
         
         int choice = _mainPage.MessageBoxUserChoice(stringBuilder.ToString());
         ItemAction action = choice == 1 ? ItemAction.Use : ItemAction.Discard;
@@ -98,6 +85,4 @@ public partial class ChestControl : UserControl
             _mainPage.Logger.Add($"Вы выкинули {equipment.Name}!");
         }
     }
-
-    
 }
